b966b38e20cd06ecb9bfb90ac6a51f630c470aae
Adam Andrewjeski <aandrewjeski@gmail.com>
2014-06-07 10:02:22 -0400

    defined both rectangles, down to only 1 test failure

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,18 +1,24 @@
+require_relative 'bounding_box'
+require 'pry'
+
 class BoundingArea
 	attr_reader :empty_area, :bottom_rect, :top_rect, :area
 
-	def initialize(empty_area = [], bottom_rect = [0.0, 0.0, 2.0, 1.0], top_rect = [2.0, 1.0, 3.0, 4.0], area = [bottom_rect, top_rect])
+	def initialize(empty_area = [], bottom_rect = nil, top_rect = nil, area = [bottom_rect, top_rect])
 		@empty_area = empty_area
 		@area = area
-		@bottom_rect = bottom_rect
 		@top_rect = top_rect
+		@bottom_rect = bottom_rect
 	end
 
 	def contains_point?(x, y)
+		@bottom_rect = BoundingBox.new(0.0, 0.0, 2.0, 1.0)
+    @top_rect = BoundingBox.new(2.0, 1.0, 3.0, 4.0)
+
 		if x = 0 && y = 0
 			false
 		end
-
+		
 	end
 
 end
